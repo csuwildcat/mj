bf41dc9b2c1c8cc762afe281325d93be68722dc5
Daniel Buchner <danieljb2@gmail.com>
2017-04-06 19:51:16 -0700

    draft

--- js/slideshow/slideshow.js
+++ js/slideshow/slideshow.js
@@ -1,59 +1,94 @@
 
 (function(){
- 
-  var images = [
-    'baby-mj-pillow.jpg',
-    'baby-mj-tugowar.jpg',
-    'mj-puppy-string.jpg'
+  
+  var last;
+  var song;
+  var loop;
+  var index = 0;
+  var imageElements = [];
+  var frag = document.createDocumentFragment();
+  var imagePaths = [
+    'baby-pillow.jpg',
+    'baby-couch.jpg',
+    'baby-standing.jpg',
+    'baby-tugowar.jpg',
+    'puppy-string.jpg',
+    'beach.jpg',
+    'in-drawer.jpg',
+    'cassie-bed-arms.jpg',
+    'couch-cassie.jpg',
+    'in-tree.jpg',
+    'cassie-wine.jpg',
+    'cassie-pregnant.jpg',
+    'syd-first-contact.jpg',
+    'corner-couch.jpg',
+    'raincoat.jpg',
+    'head-edge-bed.jpg',
+    'last-hike.jpg',
+    'seahawk-blanket.jpg',
+    'couch-with-shark.jpg',
+    'syd-laying-head.jpg',
+    'deck-with-syd.jpg', 
+    'legs-crossed-grass.jpg',
+    'grass-with-cassie.jpg',
+    'syd-hug-park.jpg',
+    'couch-with-syd.jpg'
   ];
+  var imageCount = imagePaths.length;
+  var skipFrame = fn => requestAnimationFrame(function(){
+                          requestAnimationFrame(fn)
+                        });
 
-  Sass.setWorkerUrl('../js/sass.worker.js');
-  var SassInstance = new Sass();
-  Promise.all([
+  function kenBurns() {
+    if (index == imageCount) clearInterval(loop);
+    if (last) last.setAttribute('fx', 'out');
+    (last = imageElements[index]).setAttribute('fx', 'in')
+    index++;
+  }
 
-    Promise.all(images.map(function(url){
-      return new Promise((resolve, reject) => {
-        var img = document.createElement('img');
-        img.onload = function(){ resolve(); }
-        img.onerror = function(){ reject(); }
-        img.src = '../../images/slideshow/' + url;
-      });
-    })),
+  document.addEventListener('transitionend', function(e){
+    if (e.target.getAttribute('fx') == 'out') e.target.removeAttribute('fx');
+  });
 
-    new Promise((resolve, reject) => {
-      fetch('../css/slideshow/slideshow.scss').then(response => {
-        response.text().then(scss => {
-          var options = `
-            $items: ${images.length};
-            $animation-time: 6s;
-            $transition-time: 2s;
-            $scale: 20%;
-            
-            $total-time: ($animation-time * $items);
-            $scale-base-1: (1 + $scale / 100%);
-          `;
-          SassInstance.compile(options + scss, function(result) {
-            var style = document.createElement('style');
-            style.innerHTML = result.text;
-            document.head.appendChild(style)
+Promise.all([
+
+  new Promise((resolve, reject) => {
+    song = new Audio('../../audio/see-you-again.mp3');
+    song.preload = 'auto';
+    song.oncanplay = function(){
+      resolve();
+    }
+  }),
+
+  Promise.all(imagePaths.map(function(url){
+    return new Promise((resolve, reject) => {
+      var wrap = document.createElement('div');
+          wrap.classList.add('slide');
+      var img = document.createElement('img');
+          img.onerror = function(){ reject(); }
+          img.onload = function(){
+            if (img.width > img.height) {
+              wrap.setAttribute('shape', 'wide');
+            }
+            else if (img.height > img.width) {
+              wrap.setAttribute('shape', 'tall');
+            }
             resolve();
-          });    
-        })
-      })
-    })
-
-  ]).then(response => {
-    var frag = document.createDocumentFragment();
-    images.forEach(url => {
-      var div = document.createElement('div');
-      div.className = 'slideshow-image';
-      div.style.backgroundImage = 'url(../images/slideshow/' + url + ')';
-      console.log(div.style.backgroundImage);
-      frag.appendChild(div);
+          }
+          img.src = '../../images/slideshow/' + url;
+      wrap.appendChild(img);    
+      frag.appendChild(wrap);
+      imageElements.push(wrap);
     });
-    slideshow.appendChild(frag);
-  }).catch(error => {
-    console.log(error);
-  })
+  }))
+
+]).then(response => {
+  document.body.appendChild(frag);
+  skipFrame(kenBurns);
+  loop = setInterval(kenBurns, 8000);
+  //song.play();
+}).catch(error => {
+  alert(error);
+})
 
 })();
